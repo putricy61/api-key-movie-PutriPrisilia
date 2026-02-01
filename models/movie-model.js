5aba236b7eadd8876e32dc1bebfea05dbc3edeb1
putricy61 <126140392+putricy61@users.noreply.github.com>
2026-02-01 14:04:12 +0700

    Update movie-model.js

--- models/movie-model.js
+++ models/movie-model.js
@@ -1,3 +1,15 @@
+// const { create } = require("../config/tmdb")
+
+// const createMovie = (movie)=>{
+//     return{
+//         title : movie.title,
+//         overview : movie.overview,
+//         background : movie.backdrop_path
+//     }
+// }
+
+// module.exports = createMovie
+
 const { create } = require("../config/tmdb")
 
 const createMovie = (movie)=>{
