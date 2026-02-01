4d09d982e8fa33eddf4e2beaa816c8a5935286d8
putricy61 <126140392+putricy61@users.noreply.github.com>
2026-02-01 14:03:46 +0700

    Update movie-route.js

--- routes/movie-route.js
+++ routes/movie-route.js
@@ -1,7 +1,15 @@
+// const express = require('express')
+// const movieController = require('../controllers/movie-controller')
+// const router = express.Router()
+
+// router.get('/movie',movieController.searchMovie)
+
+// module.exports = router
 const express = require('express')
 const movieController = require('../controllers/movie-controller')
 const router = express.Router()
 
-router.get('/movie',movieController.searchMovie)
+router.get('/',movieController.searchMovie)
+router.get('/popular', movieController.getPopularMovies)
 
 module.exports = router
